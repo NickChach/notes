ca192e7b4490a2fd9daca5776659a02a829630b8
NickChach <debian5user@gmail.com>
2025-10-19 21:30:24 +0300

    IO operations in JSON added, new build

--- note.go
+++ note.go
@@ -1,40 +1,54 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
 // Struct
 
 type note struct {
-	noteTitle   string
-	noteContent string
-	timeStamp   time.Time
+	NoteTitle   string
+	NoteContent string
+	TimeStamp   time.Time
 }
 
 func new(title string, content string) note {
 	return note{
-		noteTitle:   title,
-		noteContent: content,
-		timeStamp:   time.Now(),
+		NoteTitle:   title,
+		NoteContent: content,
+		TimeStamp:   time.Now(),
 	}
 }
 
 func (note note) getNoteTitle() string {
-	return "dummy text"
+	return note.NoteTitle
 }
 
 func (note note) getNoteContent() string {
-	return "dummy text"
+	return note.NoteContent
+}
+
+func (note note) save() {
+	var fileName string = strings.ReplaceAll(note.NoteTitle, " ", "_")
+	fileName = strings.ToLower(fileName)
+	fileName = fmt.Sprintf("%v.json", fileName)
+
+	json, _ := json.Marshal(note)
+	os.WriteFile(fileName, json, 0644)
 }
 
 // Utilities
 
 func promptUser(prompt string) string {
 	fmt.Println(prompt)
-	var userData string
-	fmt.Scan(&userData)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	var userData string = scanner.Text()
 	return userData
 }
 
@@ -48,4 +62,6 @@ func main() {
 
 	fmt.Println(newNote.getNoteTitle())
 	fmt.Println(newNote.getNoteContent())
+
+	newNote.save()
 }
